9021b1f359fedff564b754d7c2c7c33c4291c4d1
jonatan <jonatin@gmail.com>
2018-06-05 21:22:34 +0900

    checkpoint-0 complete

---
+++ checkpoints/checkpoint-1.md
@@ -0,0 +1,17 @@
+# Checkpoint 1 （２日目）
+
+## テックメンターが下記の項目を実装します：
+
+* [Explain] Yesterday’s homework solution code
+* [Live Coding] Introduce ExpressJS
+* [Live Coding] Start the GET server route in Express for `/api/items`.
+  * Create an items.route.js for our express routes
+  * Pseudocode the database call
+  * Note: Set up the route, but do not make a database call in it. (This will be homework)
+* [Live Coding] Set up the db connection in `connection.js`
+* [Live Coding] In the MySQL Command Line, issue a statement to list all of the items
+
+## 下記の項目を実装してみよう：
+
+* Finish the GET server route in `server/items.route.js`
+  * Use the [mysql2](https://github.com/sidorares/node-mysql2) library to retrieve items in the server route
--- server/items.sql
+++ server/items.sql
@@ -1,8 +1,12 @@
 USE `my_store`;
 
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 1', 100);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 2', 200);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 3', 300);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 4', 400);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 5', 500);
-INSERT INTO `items` (`name`, `price`) VALUES ('Item 6', 600);
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 1', 100, 'https://www.mariowiki.com/images/thumb/4/4d/Yoshi_-_Mario_Party_10.png/150px-Yoshi_-_Mario_Party_10.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 2', 200, 'https://upload.wikimedia.org/wikipedia/en/thumb/3/39/YoshiMarioParty9.png/175px-YoshiMarioParty9.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 3', 300, 'https://vignette.wikia.nocookie.net/nintendo/images/4/4f/Yoshi_Mario_Party_IT.png/revision/latest?cb=20150914001019&path-prefix=en');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 4', 400, 'https://vignette.wikia.nocookie.net/papermario/images/0/05/Yoshikid2.jpg/revision/latest?cb=20080129035230');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 5', 500, 'https://vignette.wikia.nocookie.net/mugen/images/e/e0/MarioRabbidsYoshi.png/revision/latest?cb=20170829205638');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 6', 600, 'https://vignette.wikia.nocookie.net/nintendo/images/4/48/Yoshi.png/revision/latest?cb=20140214234810&path-prefix=en');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 7', 700, 'https://vignette.wikia.nocookie.net/yoshi/images/6/68/Yoshi_Happy.png/revision/latest?cb=20150508143229');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 8', 800, 'https://c1-ebgames.eb-cdn.com.au/merchandising/images/packshots/be3a01a8c03f42c58b65f8611d9eb480_Medium.png');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 9', 900, 'http://cdn.shopify.com/s/files/1/2097/9875/products/yoshi-pic1_1024x1024.jpg?v=1503346328');
+INSERT INTO `items` (`name`, `price`, `image_url`) VALUES ('Item 10', 1000, 'https://supermariorun.com/assets/img/stage/mario03.png');
